1b993995fdfafe055388b4b79f8183f80d31b141
tanveertkd <hazarikatanveer12@gmail.com>
2021-08-01 14:32:50 +0530

    done

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # neog-markEleven
+**Lucky Birthday**  
 An app that tells you whether you're birthday is lucky or not.
+Just select your DOB from the calendar and input your lucky number to check if your birthday is lucky.
+
+Assignment: markEleven
--- app.js
+++ app.js
@@ -1,9 +1,53 @@
 const inputBirthday = document.querySelector("#input-birthday");
 const inputLuckyNumber = document.querySelector("#input-lucky-number");
 const inputBtn = document.querySelector("#input-btn");
-
+const secondaryOutput = document.querySelector(".secondary-output");
+const cookiePopup = document.querySelector(".cookie-popup");
+const cookieCloseBtn = document.querySelector("#cookie-close-btn");
 
 inputBtn.addEventListener("click", () => {
-    let date = inputBirthday.value.split("-");
-    console.log(parseInt(date.join("")));
-})
+    
+    const verified = verifyInput(inputBirthday, inputLuckyNumber);
+
+    if(verified){
+        const luck = checkIfLucky(inputBirthday, inputLuckyNumber);
+        if(luck){
+            secondaryOutput.innerText = "Yes! Your birthday is lucky!";
+        }else{
+            secondaryOutput.innerText = "Sorry! It doesn't seem to be lucky.";
+        }
+    }else{
+        secondaryOutput.innerText = "Please check your inputs."
+    }
+});
+
+const verifyInput = ((birthday, luckyNumber) => {
+    if(birthday.value === "" || luckyNumber.value === ""){
+        return false;
+    }else{
+        return true;
+    }
+});
+
+const checkIfLucky = ((date, number) => {
+    let sum = 0;
+    const datePassed = date.value.split("-");
+    let dateParsed = parseInt(datePassed.join(""));
+    const numberPassed = number.value;
+
+    while(dateParsed){
+        sum = sum+(dateParsed%10);
+        let dateD = dateParsed/10;
+        dateParsed = Math.floor(dateD);
+    }
+
+    if((sum%numberPassed)===0){
+        return true;
+    }else{
+        return false;
+    }
+});
+
+cookieCloseBtn.addEventListener("click", ()=>{
+    cookiePopup.style.display = "none";
+});
